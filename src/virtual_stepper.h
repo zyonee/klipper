63d3b6b44387144c64c39c0dae31100ce8459baa
Kevin O'Connor <kevin@koconnor.net>
2018-12-05 20:24:07 -0500

    virtual_stepper: WIP - add initial support for tracking stepper positions
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

---
+++ src/virtual_stepper.h
@@ -0,0 +1,11 @@
+#ifndef __VIRTUAL_STEPPER_H
+#define __VIRTUAL_STEPPER_H
+
+#include <stdint.h> // uint8_t
+
+struct virtual_stepper *virtual_stepper_oid_lookup(uint8_t oid);
+void virtual_stepper_stop(struct virtual_stepper *s);
+uint32_t virtual_stepper_get_position(struct virtual_stepper *s);
+void virtual_stepper_set_position(struct virtual_stepper *s, uint32_t position);
+
+#endif // stepper.h
